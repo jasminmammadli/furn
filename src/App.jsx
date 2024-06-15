26c8691bc093e4e254a69db233e5b4f174de6f0a
Yasemen <yasaman.mammadli@gmail.com>
2024-06-15 10:59:27 +0400

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./components/common/footer/Footer";
 import Header from "./components/common/header/Header";
 import Routing from "./Routing/Routing";
 import { useLocation } from "react-router-dom";
+import DataContext from "./context/DataContext";
+import axios from "axios";
 function App() {
   const { pathname } = useLocation();
-  console.log(pathname);
+  const [productData,setProductData]=useState(null)
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((res) => {setProductData(res.data),console.log(res.data)})
+      .catch(err=>console.log(err))
+  }, []);
+
   return (
-    <>
+    <DataContext.Provider value={{productData,setProductData}}>
       {pathname !== "/login" && <Header />}
       <main>
         <Routing />
       </main>
 
       {pathname !== "/login" && <Footer />}
-    </>
+    </DataContext.Provider>
   );
 }
 
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./style.css";
+import MainContextProvider from "../../../context/DataContext";
+import DataContext from "../../../context/DataContext";
+import { Link } from "react-router-dom";
 function Home() {
   let [buttons, setButtons] = useState([
     "sofa",
@@ -9,7 +12,8 @@ function Home() {
     "lightning",
     "decore",
   ]);
-  let [activeButton,setActiveButton]=useState(0)
+  let [activeButton, setActiveButton] = useState(0);
+  let { productData } = useContext(DataContext);
   return (
     <div>
       <section className="pop-prod">
@@ -24,11 +28,52 @@ function Home() {
           <div className="pop-prod-nav">
             <nav>
               {buttons.map((item, index) => (
-                <button type="button" onClick={()=>setActiveButton(index)} className={`${activeButton==index && 'active'}`} key={index}>{item}</button>
+                <button
+                  type="button"
+                  onClick={() => setActiveButton(index)}
+                  className={`${activeButton == index && "active"}`}
+                  key={index}
+                >
+                  {item}
+                </button>
               ))}
             </nav>
           </div>
+          <div className="pop-prods-wrapper">
+            {productData &&
+              productData.slice(0, 3).map((item) => (
+                <div className="card" key={item.id}>
+                  <div className="card-img">
+                    <img src={item.imgUrl} alt="" />
+                  </div>
+                  <div className="card-content">
+                    <h3>
+                      <Link to={"/product"}>{item.title}</Link>
+                    </h3>
+                    <span>${item.price}</span>
+                  </div>
+                </div>
+              ))}
+          </div>
         </div>
+      </section>
+      <section className="furniture-details">
+        <div className="furniture-img"></div>
+        <div className="furniture-detail-content">
+          <h2>best furniture manufacturer</h2>
+          <p>
+            Suspendisse varius enim in eros elementum tristique. Duis cursus, mi
+            quis viverra ornare, eros dolor interdum nulla.
+          </p>
+          <p className="last-p">
+            Suspendisse varius enim in eros elementum tristique. Duis cursus, mi
+            quis viverra ornare, eros dolor interdum nulla.
+          </p>
+          <button>discover more</button>
+        </div>
+      </section>
+      <section className="may-like">
+        <div className="may-like-head"></div>
       </section>
     </div>
   );
